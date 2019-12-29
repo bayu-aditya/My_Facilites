7c4d878fb210200a19cd5007b0b22e170d1dd3aa
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2019-12-29 17:10:53 +0700

    adding gitignore and app.py

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+# Author: Bayu Aditya
 import os
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
-TEMPLATE_DIR = os.path.abspath('frontend/templates')
-STATIC_DIR = os.path.abspath('frontend/statics')
-app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+# STATICS_DIR = os.path.abspath("statics")
+# TEMPLATES_DIR = os.path.abspath("templates")
 
-# GET: homepage /
-@app.route('/')
-def homepage():
-    return render_template("index.html")
+# app = Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATICS_DIR)
+app = Flask(__name__)
 
-@app.route('/register')
-def register():
-    return render_template("set_account.html")
+items = [
+    {
+        "name": "buku",
+        "start": "27 Des 2019",
+        "end": "28 Des 2019"
+    },
+    {
+        "name": "tas",
+        "start": "27 Aug 2019",
+        "end": "28 Aug 2019"
+    }
+]
 
-# if __name__ == "__main__":
-app.run(port=5000)
+# API Services
+@app.route('/api/items')
+def list_items():
+    return jsonify({"items": items})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
