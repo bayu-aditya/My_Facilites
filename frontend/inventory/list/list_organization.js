b3878dcbf15a448d93ba570272a6a0e9b7affa09
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-01-05 23:03:13 +0700

    Adding edit and delete button

--- frontend/inventory/list/list_organization.js
+++ frontend/inventory/list/list_organization.js
@@ -20,11 +20,21 @@ function response_to_table(response) {
     for (var i=0; i<list.length; i++) {
         var tr = document.createElement("tr"),
             td_user = document.createElement("td"),
-            td_name = document.createElement("td");
+            td_name = document.createElement("td"),
+            td_btn = document.createElement("td"),
+            btn_group = document.createElement("div"),
+            btn_edit = document.createElement("button"),
+            btn_delete = document.createElement("button");
         td_user.innerHTML = list[i].user;
         td_name.innerHTML = list[i].name;
+        btn_group.className = "btn-group";
+        btn_edit.className = "btn btn-info"; btn_edit.innerHTML = "edit"; btn_edit.id = "edit_" + list[i].name;
+        btn_delete.className = "btn btn-danger"; btn_delete.innerHTML = "delete"; btn_delete.id = "delete_" + list[i].name;
+        
+        btn_group.appendChild(btn_edit); btn_group.appendChild(btn_delete);
         tr.appendChild(td_user);
         tr.appendChild(td_name);
+        tr.appendChild(btn_group);
         tab_body.appendChild(tr);
     }
 }
