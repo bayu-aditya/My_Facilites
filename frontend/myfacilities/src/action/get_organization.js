76b0ef9b7e2b58abf87f56ece737e27f6616a8e7
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-01-17 14:46:35 +0700

    getting list of organization using fetch

--- frontend/myfacilities/src/action/get_organization.js
+++ frontend/myfacilities/src/action/get_organization.js
@@ -1,15 +1,40 @@
+import $ from 'jquery';
+
+// var admin = "bayu_aditya";
+// var API = "http://0.0.0.0:8888/organizations";
+// var url = API + "?admin=" + admin;
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//         var resp = JSON.parse(this.responseText);
+//         // console.log(resp);
+//         show_organization();
+//     }
+// }
+// xhr.open("GET", url, true);
+// xhr.send();
+
+// function show_organization() {
+//     let body = document.getElementById("body_tb_org");
+//     body.innerHTML = "test"
+// }
+
+
+
 // contoh respon dari API
 var data = {
     "organization": [
         {
-            "id": "123456",
+            "_id": "123456",
             "name": "Fakultas Ilmu Komputer"
         },
         {
-            "id": "789123",
+            "_id": "789123",
             "name": "Computational and Experimental Nanoscience"
         }
     ]
 }
 
-export {data};
+// // export {get_list_organization};
+// export {data};
--- frontend/myfacilities/src/component/list_inventory.jsx
+++ frontend/myfacilities/src/component/list_inventory.jsx
@@ -2,17 +2,66 @@ import React from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import Adding_org from './adding_organization';
-import {data} from '../action/get_organization.js';
 import { IconButton } from '@material-ui/core';
+
+var admin = "bayu_aditya";
+var API = "http://0.0.0.0:8888/organizations";
+var url = API + "?admin=" + admin;
 
 export class Table_list_organization extends React.Component {
     constructor(props) {
         super(props)
-        this.data = data["organization"]
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(url)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    data: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        const {error, isLoaded, data} = this.state;
+        if (isLoaded) {
+            if (error) {
+                return <div>Something Wrong in Server: {error.message}</div>
+            }
+            else {
+                return <List_organization data={data} />
+            }
+        }
+        else {
+            return <div>Loading...</div>
+        }
+    }
+}
+
+
+class List_organization extends React.Component {
+    constructor(props) {
+        super(props)
+        this.data = this.props.data["organization"]
     }
     selectHandler = (e) => {
         let idx = e.target.id;
-        console.log("clicked ID: " + this.data[idx]["id"] + " Name: " + this.data[idx]["name"])
+        console.log("clicked ID: " + this.data[idx]["_id"] + " Name: " + this.data[idx]["name"])
     }
     row(name, idx) {
         return (
