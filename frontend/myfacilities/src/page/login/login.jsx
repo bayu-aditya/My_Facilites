142298c3fc09bcc4f74523b8711b8fb6e05cc63d
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-01-20 19:44:38 +0700

    adding rules in login page

--- frontend/myfacilities/src/page/login/login.jsx
+++ frontend/myfacilities/src/page/login/login.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { Grid, TextField, Button, Container, Box } from '@material-ui/core';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import LockIcon from '@material-ui/icons/Lock';
-import './login.scss';
 import { Redirect } from 'react-router-dom';
-
-// const useStyle = makeStyles(theme => ({
-//     margin: {
-//         margin: theme.spacing(3)
-//     }
-// }));
-const defaultProps = {
-    bgcolor: 'background.paper',
-    border: 3,
-    m: 1,
-    borderColor: 'text.primary',
-  };
+import Dialog from '@material-ui/core/Dialog';
+import './login.scss';
+import { DialogTitle, DialogContent, DialogContentText } from '@material-ui/core';
 
 export class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            auth: false,
+            authenticate: false,
+            responseError: null,
+            dialogOpen: false,
             username: null,
             password: null
         }
@@ -31,67 +20,89 @@ export class Login extends React.Component {
     }
     loginHandle = (e) => {
         e.preventDefault();
-        // console.log(this.state)
-        let values = {
+        var values = {
             "username": this.state.username,
             "password": this.state.password
         }
-        fetch("http://0.0.0.0:8888/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(values)
-        })
-        .then((response) => {
-            if (response.status >= 200 && response.status <= 299) {
-                return response.json()
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then((jsonResponse) => {
-            document.cookie = "access_token=" + jsonResponse["access_token"]
-            this.setState({auth: true})
-        }).catch((error) => {
-            console.log("error: " + error);
-        });
+        console.log(values)
+        this.setState({auth: true})
+        var self = this;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            console.log("waiting ...")
+            if (this.readyState === 4) {
+                // success
+                if (this.status === 202) {
+                    console.log(this.responseText);
+                    // get cookies
+                    self.authHandler();
+                }
+                // wrong username
+                else if (this.status === 401) {
+                    let resp = JSON.parse(this.responseText);
+                    self.setState({responseError: resp["message"]})
+                    self.openDialog();
+                }
+                // username not found
+                else if (this.status === 404) {
+                    let resp = JSON.parse(this.responseText);
+                    self.setState({responseError: resp["message"]})
+                    self.openDialog();
+                // something wrong in server
+                } else {
+                    console.log("server error")
+                }
+            }}
+        xhttp.open("POST", "http://0.0.0.0:8888/login");
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.send(JSON.stringify(values));
     }
     toDashboard = () => {
-        if (this.state.auth) {
-            return <Redirect to="/dashboard" />
+        if (this.state.authenticate) {
+            console.log("login successful, to dashboard")
+            return <Redirect to="/dashboard_dummy" />
         }
+    }
+    authHandler = () => {
+        this.setState({authenticate: true})
+    }
+    openDialog = () => {
+        this.setState({dialogOpen: true})
+    }
+    closeDialog = () => {
+        this.setState({dialogOpen: false})
+    }
+    dialogHandle = () => {
+        return (
+        <Dialog open={this.state.dialogOpen} onClose={this.closeDialog}>
+            <DialogTitle>Authorization</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {this.state.responseError}
+                </DialogContentText>
+            </DialogContent>
+        </Dialog>)
     }
     render() {
         return (
-            <div>
+            <div id="screen">
                 {this.toDashboard()}
-                <Container maxWidth="xs">
-                    <Box border={0} {...defaultProps}>
-                        <h3>Myfacilities Login</h3>
-                        <form autoComplete="off" onSubmit={this.loginHandle}>
-                            <div>
-                                <Grid container spacing={1} alignItems="flex-end">
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField required id="username" label="username" onChange={this.changeHandle}/>
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <div>
-                                <Grid container spacing={1} alignItems="flex-end">
-                                    <Grid item>
-                                        <LockIcon />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField required id="password" label="password" type="password" onChange={this.changeHandle}/>
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <Button type="submit">Login</Button>
-                        </form>
-                    </Box>
-                </Container>
+                {this.dialogHandle()}
+                <div className="container-xl login">
+                    <h3 className="header">Myfacilities Login</h3>
+                    <div className="dropdown-divider"></div>
+                    <form autoComplete="off" onSubmit={this.loginHandle}>
+                        <div>
+                            <input id="username" required className="form-control" placeholder="username" onChange={this.changeHandle}></input>
+                        </div>
+                        <div>
+                            <input id="password" required type="password" className="form-control" placeholder="password" onChange={this.changeHandle}></input>
+                        </div>
+                        <div className="right">
+                            <button type="submit" className="btn btn-outline-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
