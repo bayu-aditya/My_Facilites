04bd13a5ba03fe8bb80be5224f788f51704ec68a
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-02-10 14:15:29 +0700

    Adding resgister and login pages to views folder

--- frontend/myfacilities/src/App.js
+++ frontend/myfacilities/src/App.js
@@ -7,18 +7,18 @@ import {
 import { RouteWithLayout } from './component';
 import Main from './layout';
 import { 
+    Login,
+    Register,
     Dashboard as Next_Dashboard,
     Inventory as Next_Inventory,
     Timeline as Next_Timeline } from './views';
 
-import Dashboard from './page/dashboard/organization';
-import Inventory from './page/inventory/inventory';
-import Timeline from './page/timeline/timeline';
-import Login from './page/login/login';
+// import Dashboard from './page/dashboard/organization';
+// import Inventory from './page/inventory/inventory';
+// import Timeline from './page/timeline/timeline';
 import {Provider} from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import storeAndPersistor from './store';
-import Register from './page/register/register';
 
 const { store, persistor } = storeAndPersistor();
 
@@ -29,6 +29,14 @@ class App extends React.Component {
                 <PersistGate loading={null} persistor={persistor}>
                 <Router>
                     <Switch>
+                        <Route 
+                            component={Login} 
+                            exact 
+                            path="/"/>
+                        <Route 
+                            component={Register} 
+                            exact 
+                            path="/register" />
                         <RouteWithLayout
                             layout={Main}
                             component={Next_Dashboard}
@@ -48,11 +56,9 @@ class App extends React.Component {
                             path="/next_dashboard/next_inventory/next_timeline"
                         />
 
-                        <Route exact path="/" component={Login} />
-                        <Route exact path="/register" component={Register} />
-                        <Route exact path="/dashboard" component={Dashboard} />
+                        {/* <Route exact path="/dashboard" component={Dashboard} />
                         <Route exact path="/dashboard/inventory" component={Inventory} />
-                        <Route exact path="/dashboard/inventory/timeline" component={Timeline} />
+                        <Route exact path="/dashboard/inventory/timeline" component={Timeline} /> */}
                     </Switch>
                 </Router>
                 </PersistGate>
